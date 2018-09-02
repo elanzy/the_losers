99b4b30edafa7516c6c8fe6bf1ff67ee69bcefd8
Elanzy Reyes <makiztiam@hotmail.com>
2018-09-02 00:31:14 -0400

    add function to push loser input  in the losers array

--- main/script.js
+++ main/script.js
@@ -1,14 +1,23 @@
 (function Loser() {
+  this.losers = [];
+
   this.init = function () {
     this.addLoser();
   }
 
   this.addLoser = function() {
     let addBtn = document.querySelector("#add_loser")
+    let loserValue = document.querySelector("#loser_value");
 
+    function addTheLoser() {
+      let input = loserValue.value;
+      loserValue.value = "";
+
+      losers.push(input);
+     }
 
     addBtn.addEventListener("click", function (e) {
-      console.log("click");
+      addTheLoser();
      })
   }
 
