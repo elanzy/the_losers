3b8a171ce68d1e7374b1c5bac63993d60359c554
Elanzy Reyes <makiztiam@hotmail.com>
2018-09-02 17:58:01 -0400

    post input value

--- main/script.js
+++ main/script.js
@@ -5,20 +5,32 @@
     this.addLoser();
   }
 
-  this.addLoser = function() {
+  this.addLoser = function () {
     let addBtn = document.querySelector("#add_loser")
     let loserValue = document.querySelector("#loser_value");
 
     function addTheLoser() {
       let input = loserValue.value;
       loserValue.value = "";
-
       losers.push(input);
-     }
+      this.loserList();
+    }
 
     addBtn.addEventListener("click", function (e) {
       addTheLoser();
-     })
+    })
+  }
+
+  this.loserList = function () {
+    let loserWrapper = document.querySelector(".loser_list_wrapper")
+    let template = "";
+
+    for (loser of losers) {
+      template += `<span class="loser-name" data-id="">${loser}</span>`
+    }
+
+    loserWrapper.innerHTML = "";
+    loserWrapper.insertAdjacentHTML("afterbegin", template);
   }
 
   this.init();
